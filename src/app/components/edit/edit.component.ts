b622ccf196a4dca52d611df7d8bb33c2e6c8411c
Eric <56220682+ThEricsson@users.noreply.github.com>
2022-08-16 19:51:26 +0200

    one little fix

--- src/app/components/edit/edit.component.ts
+++ src/app/components/edit/edit.component.ts
@@ -34,7 +34,7 @@ export class EditComponent implements OnInit {
     private _route: ActivatedRoute,
     private _uploadService: UploadService
   ) { 
-    this.title = 'Editar proyecte'
+    this.title = 'Editar projecte'
   }
 
   //A l'inici recollim les dades amb la funció getProject
